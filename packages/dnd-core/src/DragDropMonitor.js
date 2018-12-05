cbc9f103c673d24c9f4cfd8573166c56092bebe4
Nassim Benkirane <nassim.benkirane@gmail.com>
2018-12-05 18:06:11 +0100

    updates

--- packages/dnd-core/src/DragDropMonitor.js
+++ packages/dnd-core/src/DragDropMonitor.js
@@ -14,7 +14,6 @@ export default class DragDropMonitor {
 		this.registry = new HandlerRegistry(store)
 	}
 
-
 	subscribeToStateChange(listener, options = {}) {
 		const { handlerIds } = options
 		invariant(typeof listener === 'function', 'listener must be a function.')
